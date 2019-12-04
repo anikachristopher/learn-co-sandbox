852d1eb023f9ada43c4e61719fc44dd2c5a10e00
Anika Christopher <anika.mchristopher@gmail.com>
2019-12-04 03:45:46 +0000

    modified cli.rb and also added additional files to the bin and lib folders

--- cli_project/lib/cli_project/cli.rb
+++ cli_project/lib/cli_project/cli.rb
@@ -1,6 +1,12 @@
 class CLIProject::CLI
   
   def call
-    puts "Welcome to the Meals Database. Choose a category and see the available dishes"
+    puts "Welcome to the Meals Database! Choose a category to see the available dishes"
   end 
+  
+  meals
+  category 
+  
 end 
+  
+  def meals
